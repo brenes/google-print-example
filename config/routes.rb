4d9bfed8616c395d0438855eaed60789b13f569e
David J. Brenes <davidjbrenes@gmail.com>
2016-09-17 15:10:44 +0200

    Loading the printer and creating a submit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :printers
+  resources :printers, only: [:index, :show] do
+    resources :print_jobs, only: [:create, :show]
+  end
+
   root to: 'setup#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
